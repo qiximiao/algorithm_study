b0d883e305ec5e5ba7c6cb859c231d3e32085d97
zhaomin <zhaomindedi@163.com>
2018-12-04 14:21:32 +0800

    LeetCode题库1,2,3

--- src/main/java/LeetCode_3.java
+++ src/main/java/LeetCode_3.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -9,40 +11,40 @@ import java.util.Set;
  */
 public class LeetCode_3 {
     //暴力法
-//    public int lengthOfLongestSubstring2(String s) {
-//        String result = "";
-//        String a = "";
-//        for (int i = 0; i < s.length(); i++) {
-//            a = ""+s.charAt(i);
-//            if (result.length() == 0) {
-//                result = String.valueOf(s.charAt(i));
-//            }
-//
-//            for (int j = i + 1; j < s.length(); j++) {
-//                if (isContain2(a,s.charAt(j))) {//
-//                    break;
-//                } else {
-//                    a = a + s.charAt(j);
-//                    if (a.length() > result.length()) {
-//                        result = a;
-//                    }
-//                }
-//            }
-//        }
-//
-//        return result.length();
-//    }
-//
-//    private boolean isContain2(String a, char c){
-//        Set<Character> set = new HashSet<>();
-//        for(int i=0;i<a.length();i++){
-//            set.add(a.charAt(i));
-//        }
-//        if(set.contains(c)){
-//            return true;
-//        }
-//        return false;
-//    }
+    public int lengthOfLongestSubstring1(String s) {
+        String result = "";
+        String a = "";
+        for (int i = 0; i < s.length(); i++) {
+            a = "" + s.charAt(i);
+            if (result.length() == 0) {
+                result = String.valueOf(s.charAt(i));
+            }
+
+            for (int j = i + 1; j < s.length(); j++) {
+                if (isContain1(a, s.charAt(j))) {//
+                    break;
+                } else {
+                    a = a + s.charAt(j);
+                    if (a.length() > result.length()) {
+                        result = a;
+                    }
+                }
+            }
+        }
+
+        return result.length();
+    }
+
+    private boolean isContain1(String a, char c) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < a.length(); i++) {
+            set.add(a.charAt(i));
+        }
+        if (set.contains(c)) {
+            return true;
+        }
+        return false;
+    }
 
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
@@ -64,6 +66,50 @@ public class LeetCode_3 {
         return true;
     }
 
+    //减少复杂度的方法
+    public int lengthOfLongestSubstring3(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();//移动窗口
+        int i = 0, j = 0, ans = 0;
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))) {//set中一直不包含重复的,j碰到重复的,将i的位置提前到被重复符号的位置
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+    //针对3的优化,i一次找到位置
+    public int lengthOfLongestSubstring4(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+
+    //独立却ASCII 128
+    @Test
+    public int lengthOfLongestSubstring5(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);//s.charAt(j)字符,字符的index[ASCII] ,第一次i不动,如果重复,提前提前到该位置
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;//赋予位置
+        }
+        return ans;
+    }
 
     @Test
     public void test() {
