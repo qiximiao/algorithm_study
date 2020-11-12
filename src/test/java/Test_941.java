053df3179ecbd8465da83de307aa49a7d330a662
qiximiao <zhaomindedi@gmail.com>
2020-11-12 23:33:07 +0800

    提交

---
+++ src/test/java/Test_941.java
@@ -0,0 +1,15 @@
+import leetcode.leetcode.editor.cn.leetcode.editor.cn.Leetcode_941;
+import org.junit.Test;
+
+/**
+ * @Author qiximiao
+ * @Date 2020-11-03
+ */
+public class Test_941 {
+    @Test
+    public void test(){
+        Leetcode_941 test = new Leetcode_941();
+        test.validMountainArray(new int[]{9,8,7,6,5});
+
+    }
+}
