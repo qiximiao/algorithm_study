e06fa1a912c5cdb20268f819d040e884c0f21eac
qiximiao <zhaomindedi@gmail.com>
2020-12-07 00:49:15 +0800

    bug提交

--- src/main/java/排序/QuickSort.java
+++ src/main/java/排序/QuickSort.java
@@ -11,27 +11,29 @@ import java.util.List;
 public class QuickSort {
     public static void main(String[] args) {
         int[] arr = {4, 1, 3, 7, 2};
-        quickSort(arr,0,4);
-        System.out.println(arr);
+        int[] arr2 = {4, 1, 3, 7, 2};
+        quickSort(arr, 0, arr.length - 1);
+        QuickSortTest(arr2, 0, arr.length - 1);
+        System.out.println("abc");
     }
 
     /**
      * @param arr    数组
-     * @param lIndex 左下标
-     * @param rIndex 右下标
+     * @param start 左下标
+     * @param end 右下标
      */
-   static void quickSort(int[] arr, int lIndex, int rIndex) {
-        if (lIndex >= rIndex) {
+    static void quickSort(int[] arr, int start, int end) {
+        if (start > end) {
             return;
         }
-        int nowL = lIndex, nowR = rIndex;
-        int key = arr[nowL];//待比较的值
+        int nowL = start, nowR = end;
+        int key = arr[start];//待比较的值
         while (nowL < nowR) {//一旦跳出循环nowL和nowR应该是相等的，内循环相等的时候就不执行了
-            while (nowL < nowR && arr[nowR] > key) {//找到比arr[]lIndex大的
+            while (nowL < nowR && arr[nowR] >= key) {//找到比arr[]lIndex大的
                 nowR--;
             }
 
-            while (nowL < nowR && arr[nowL] < key) {//找到比arr[]lIndex小的
+            while (nowL < nowR && arr[nowL] <= key) {//找到比arr[]lIndex小的
                 nowL++;
             }
             if (nowL < nowR) {//只有在两边都找到了可交换的值时才会进入
@@ -48,14 +50,47 @@ public class QuickSort {
 
 
         //最后将基准为与i和j相等位置的数字交换
-        arr[lIndex] = arr[nowL];
+        arr[start] = arr[nowL];
         arr[nowL] = key;
         //递归调用左半数组
-        quickSort(arr, lIndex, nowR - 1);
+        quickSort(arr, start, nowR - 1);
         //递归调用右半数组
-        quickSort(arr, nowL + 1, rIndex);
+        quickSort(arr, nowR + 1, end);
 
 
     }
 
+
+    public static void QuickSortTest(int arr[], int start, int end) {
+        int temp, i, j, t;
+        if (start > end) {
+            return;
+        }
+        temp = arr[start];
+        i = start;
+        j = end;
+        while (i < j) {
+            //在右边寻找比基数temp小的数
+            while (temp <= arr[j] && i < j) {
+                j--;
+            }
+            //在左边寻找比基数temp大的数
+            while (temp >= arr[i] && i < j) {
+                i++;
+            }
+            //符合条件就交换
+            if (i < j) {
+                t = arr[j];
+                arr[j] = arr[i];
+                arr[i] = t;
+            }
+        }
+        //将基数与和i,j相遇的索引交换值
+        arr[start] = arr[i];
+        arr[i] = temp;
+        //递归调用左边
+        QuickSortTest(arr, start, j - 1);
+        //递归调用右边
+        QuickSortTest(arr, i + 1, end);
+    }
 }
